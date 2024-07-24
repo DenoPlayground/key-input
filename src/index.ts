e7694bea4ba805f032cd22d52acb59c865be3c21
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-07-24 11:28:36 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 Deno.stdin.setRaw(true)
-// Deno.stdout.write(new TextEncoder().encode('\x1b[?1003h\x1b[?1006h\x1b[2J\x1b[H\x1b[?25l'));
+Deno.stdout.write(new TextEncoder().encode('\x1b[?1003h\x1b[?1006h\x1b[2J\x1b[H\x1b[?25l'));
 
 for await (const keys of Deno.stdin.readable) {
   
-  console.log(`keyMap.set([${keys.join(', ')}], {code: '', key: '${String.fromCharCode(...Array.from(keys))}'})`);
-  
+  console.log(`keyMap.set([${keys.join(', ')}], {code: '', key: '${String.fromCharCode(...Array.from(keys.slice(1)))}'})`);
+  if (keys[0] === 3) break;
 }
+
+Deno.stdout.write(new TextEncoder().encode('\x1b[?1003l\x1b[?1006l\x1b[?25h'));
