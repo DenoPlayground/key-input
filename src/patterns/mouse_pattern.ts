659353478f5f0a8bf633961590974e395b3a1a81
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-07-24 13:27:46 +0200

    moved

--- src/patterns/mouse_pattern.ts
+++ src/patterns/mouse_pattern.ts
@@ -1,4 +1,4 @@
-export const patterns = {
+export const mousePattern = {
   MOUSE_MOVE: /\[<(?<buttons>35|43|51|59);(?<column>\d+);(?<row>\d+)m/,
   MOUSE_DRAG: /\[<(?<buttons>32|33|34|40|41|42|48|49|50|56|57|58);(?<column>\d+);(?<row>\d+)M/,
   MOUSE_DOWN: /\[<(?<buttons>0|1|2|8|9|10|16|17|18|24|25|26);(?<column>\d+);(?<row>\d+)M/,
