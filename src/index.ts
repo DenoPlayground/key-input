0c20f57c49607eef25f7e64c13855e4fb970fb2f
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-07-24 14:55:52 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,28 @@ import { mousePattern } from './patterns/mouse_pattern.ts';
 Deno.stdin.setRaw(true)
 Deno.stdout.write(new TextEncoder().encode('\x1b[?1003h\x1b[?1006h\x1b[2J\x1b[H\x1b[?25l'));
 
-for await (const keys of Deno.stdin.readable) {
-  const chunk = new TextDecoder().decode(keys.slice(1))
-  console.log(mousePattern.MOUSE_MOVE.exec(chunk)?.groups);
+// console.log(new TextDecoder('utf-8').decode(new Uint8Array([132])));
+console.log(new TextEncoder().encode('äöü'));
+Deno.stdout.write(new TextEncoder().encode('äöü'))
+
+for await (const iterator of Deno.stdin.readable.values()) {
+  console.log(iterator);
   
-  // console.log(`keyMap.set([${keys.join(', ')}], {code: '', key: '${String.fromCharCode(...Array.from(keys.slice(1)))}'})`);
+}
+
+
+for await (const keys of Deno.stdin.readable) {
+  const chunk = new TextDecoder().decode(keys[0] === 0x1b ? keys.slice(1) : keys)
+
+  console.log(chunk, keys);
+
+  
+  // if (mousePattern.MOUSE_MOVE.exec(chunk)) {
+  //   console.log(mousePattern.MOUSE_MOVE.exec(chunk)?.groups);
+  // } else {
+  //   console.log(`${new TextDecoder('ascii').decode(keys[0] === 0x1b ? keys.slice(1) : keys)} ${keys} \\u${keys[0].toString(16).padStart(4, '0')}`);
+  // }
+  
   if (keys[0] === 3) break;
 }
 
