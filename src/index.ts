b67e067c3a8a0abaaa49dc2309b2fcddc826b9bc
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-07-24 13:27:52 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { patterns } from './event/mouse/patterns.ts';
+import { mousePattern } from './patterns/mouse_pattern.ts';
 
 Deno.stdin.setRaw(true)
 Deno.stdout.write(new TextEncoder().encode('\x1b[?1003h\x1b[?1006h\x1b[2J\x1b[H\x1b[?25l'));
 
 for await (const keys of Deno.stdin.readable) {
   const chunk = new TextDecoder().decode(keys.slice(1))
-  console.log(chunk, patterns.MOUSE_MOVE.exec(chunk)?.groups);
+  console.log(mousePattern.MOUSE_MOVE.exec(chunk)?.groups);
   
   // console.log(`keyMap.set([${keys.join(', ')}], {code: '', key: '${String.fromCharCode(...Array.from(keys.slice(1)))}'})`);
   if (keys[0] === 3) break;
