4f3d16032b092dd7c92ad59fa79085a3ec8465b8
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-07-24 14:55:17 +0200

    Update mouse_pattern.ts

--- src/patterns/mouse_pattern.ts
+++ src/patterns/mouse_pattern.ts
@@ -1,6 +1,8 @@
 export const mousePattern = {
   MOUSE_MOVE: /\[<(?<buttons>35|43|51|59);(?<column>\d+);(?<row>\d+)m/,
   MOUSE_DRAG: /\[<(?<buttons>32|33|34|40|41|42|48|49|50|56|57|58);(?<column>\d+);(?<row>\d+)M/,
-  MOUSE_DOWN: /\[<(?<buttons>0|1|2|8|9|10|16|17|18|24|25|26);(?<column>\d+);(?<row>\d+)M/,
-  MOUSE_UP: /\[<(?<buttons>0|1|2|8|9|10|16|17|18|24|25|26);(?<column>\d+);(?<row>\d+)m/,
-}
+  MOUSE_BUTTON_DOWN: /\[<(?<buttons>0|1|2|8|9|10|16|17|18|24|25|26);(?<column>\d+);(?<row>\d+)M/,
+  MOUSE_BUTTON_UP: /\[<(?<buttons>0|1|2|8|9|10|16|17|18|24|25|26);(?<column>\d+);(?<row>\d+)m/,
+  MOUSE_SCROLL_DOWN: / /,
+  MOUSE_SCROLL_UP: / /,
+} as const;
